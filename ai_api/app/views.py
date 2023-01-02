91765d1eaddf811c330b46a2838c4f8e12bc7ecc
carlitos-206 <carloscaceres041@gmail,com>
2023-01-02 03:51:07 -0800

    repo update

--- ai_api/app/views.py
+++ ai_api/app/views.py
@@ -32,9 +32,25 @@ app = firebase_admin.initialize_app(cred)
 # Call the Firbase DB
 db = firestore.client()
 
+@csrf_exempt
 def index(request):
-  # I will create a session to ensure the user has agreed to the cookies to acces the ai
-  return HttpResponse('set the session')
+  if request.method == "POST":
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    
+    request.session['user_id'] = body['data_id']
+    apiResponse = {
+      "Status": 200,
+      "AI": "Active"
+    }
+    return JsonResponse(apiResponse)
+  else:
+    apiResponse = {
+      "Status": 400,
+      "AI": ""
+    }
+    return JsonResponse(apiResponse)
+    
 # Since theres no important data being passed no csrf_token is need
 @csrf_exempt
 def imgAiCreation(request):
@@ -43,41 +59,61 @@ def imgAiCreation(request):
     # Parsing the request
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
-    # Request ID
-    id = uuid.uuid1()
-    
-    # Calling the OpenAi API
-    response = openai.Image.create(
-      prompt= '%s' % body['prompt'],
-      n=1,
-      size="1024x1024"
-      )
-    image_url = response['data'][0]['url']
+    # The user must have a active session to access ai
+    if body['prompt'] or body['data_id'] not in body:
+      api_response = {
+        "status": 500,
+        "error":"No Match Found",
+        'date': datetime.now().strftime("%m/%d/%Y %H:%M:%S %z") # the %z is for utc offset
+      }
+      return JsonResponse(api_response)
+    else:
+      if 'user_id' in request.session:
+        # Request ID
+        id = uuid.uuid1()
+        # This ensure the user is the same as the backend session
+        if request.session['user_id'] == body['data_id']:
+          # Calling the OpenAi API
+          response = openai.Image.create(
+            prompt= '%s' % body['prompt'],
+            n=1,
+            size="1024x1024"
+            )
+          image_url = response['data'][0]['url']
+          print(image_url)
+          # Adding the data to Firebase
+          doc_ref = db.collection(u'openAI_img_request').document(u'{}'.format(id))
+          doc_ref.set({
+            u'URL': u'%s' % image_url, 
+            u'id': u'{}'.format(id),
+            u'data_id': u'{}'.format(body['data_id']),
+            u'prompt': u'%s' % body['prompt'],
+            u'date': u'{}'.format(datetime.now().strftime("%m/%d/%Y %H:%M:%S %z"))
+          })
 
-    # Adding the data to Firebase
-    doc_ref = db.collection(u'openAI_img_request').document(u'{}'.format(id))
-    doc_ref.set({
-      u'URL': u'%s' % image_url, 
-      u'id': u'{}'.format(id),
-      u'data_id': u'{}'.format(body['data_id']),
-      u'prompt': u'%s' % body['prompt'],
-      u'date': u'{}'.format(datetime.now().strftime("%m/%d/%Y %H:%M:%S %z"))
-    })
-    
-    # Front-End return package 
-    api_response = {
-      'status':200,
-      'id': id,
-      'Prompt': '%s' % body['prompt'],
-      'OpenAi_url': image_url,
-      'date': datetime.now().strftime("%m/%d/%Y %H:%M:%S %z") # the %z is for utc offset
-    }
-    return JsonResponse(api_response)
+          # Front-End return package 
+          api_response = {
+            'status':200,
+            'id': id,
+            'Prompt': '%s' % body['prompt'],
+            'OpenAi_url': image_url,
+            'date': datetime.now().strftime("%m/%d/%Y %H:%M:%S %z") # the %z is for utc offset
+          }
+          return JsonResponse(api_response)    
+        else:
+          api_response = {
+            "status": 400,
+            "id": id,
+            'error': 'User Not Valid',
+            'date': datetime.now().strftime("%m/%d/%Y %H:%M:%S %z") # the %z is for utc offset
+          }
+          return  JsonResponse(api_response)
   else:
     # Front-End return package 
     api_response = {
       'status': 400,
-      'type': 'Bad Request',
+      'id': id,
+      'error': 'Bad Request',
       'date': datetime.now().strftime("%m/%d/%Y %H:%M:%S %z") # the %z is for utc offset
     }
     return JsonResponse(api_response)
